f925b9addcda27750ca645c1f3c876191762ff77
shangying <shangying@uccc.cc>
2018-04-12 18:47:26 +0800

    注释语句

---
+++ src/HelloWorld.java
@@ -0,0 +1,26 @@
+/**
+ * name:尚莹
+ * Created by uccc on 2018/4/12.
+ */
+public class HelloWorld {
+    public static void main (String[] args)
+    {
+        System.out.println("HelloWorld");
+
+        System.out.println("尚莹编辑");
+
+        //编译运行时该句可以实现简单的输入
+
+        /*编译运行：
+         *javac HelloWorld.java
+         *java HelloWorld 输入内容及输出
+
+         *输出结果：
+         * HelloWorld
+         * 尚莹编辑
+         * 输入内容及输出
+         */
+
+
+    }
+}
